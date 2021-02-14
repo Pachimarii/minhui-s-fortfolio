eaba8e6b56eb509dc4b389271738c9f0463ccfb9
Minhui Xie <MXie@cese.fraunhofer.org>
2021-02-13 22:53:52 -0500

    _

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
       'Minhui Xie is a web developeI am a web developer graduated from University of Maryland in Dec, 2020; who is always keeping the good coding habits and currently specializing in front end development. I am always excited about learning and exploring new technologies, therefore I would be more than happy to adapt to any tools that are required! :Dr specializing in frontend development.',
     siteUrl: 'https://minhui-xie.vercel.app', // No trailing slash allowed!
     image: '/home.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: '_',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
